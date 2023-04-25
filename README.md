8de21e6cee97c1871602a2e09a8cb8269c1be30a
ericdwkim <ericdwkim.io@gmail.com>
2023-04-24 21:34:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+To automate responses for Google Business Reviews using Python, you can use the Google My Business API. This API allows you to manage and respond to reviews programmatically. Additionally, you can use a sentiment analysis library like TextBlob to analyze the content of user's responses and decide on an appropriate reply. Here's a high-level overview of the steps you need to take:
+
+    Set up a Google My Business API project and enable the API:
+        Follow the instructions in the official documentation to set up a project, enable the API, and obtain API credentials (OAuth 2.0 client ID and secret).
+
+    Install the required Python libraries:
+
+`pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client textblob`
+
+Authenticate and obtain an API client:
+Use the official guide to authenticate using the client ID and secret you obtained earlier. Once authenticated, you can obtain an API client object to interact with the Google My Business API.
+
+Fetch the reviews:
+Use the API client to fetch the Google Business Reviews for your account. You can use the accounts.locations.reviews.list method to achieve this.
+
+Analyze the sentiment and respond to reviews:
+Loop through the reviews, use TextBlob to analyze the sentiment of the review content, and decide on an appropriate reply based on the number of stars and the sentiment. Use the accounts.locations.reviews.reply method to reply to the reviews.
---
+++ main.py
@@ -0,0 +1,45 @@
+import google.auth
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from textblob import TextBlob
+
+# Authenticate and obtain an API client
+creds = None
+if creds and creds.expired and creds.refresh_token:
+    creds.refresh(Request())
+else:
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=["https://www.googleapis.com/auth/business.manage"])
+    creds = flow.run_local_server(port=0)
+
+api_client = build("mybusiness", "v4", credentials=creds)
+
+# Replace with your Google My Business account and location IDs
+account_id = "your-account-id"
+location_id = "your-location-id"
+
+# Fetch the reviews
+reviews = api_client.accounts().locations().reviews().list(parent=f"accounts/{account_id}/locations/{location_id}").execute()
+
+# Analyze sentiment and respond to reviews
+for review in reviews["reviews"]:
+    user_name = review["reviewer"]["displayName"]
+    num_of_stars_user_gave = review["starRating"]
+    review_text = review["comment"]
+    
+    # Analyze sentiment using TextBlob
+    sentiment = TextBlob(review_text).sentiment.polarity
+    response = ""
+
+    if sentiment > 0 and num_of_stars_user_gave >= 4:
+        response = f"Thanks for the {num_of_stars_user_gave} stars, {user_name}! We hope you'll be back soon."
+    else:
+        missing_stars = 5 - num_of_stars_user_gave
+        response = f"Sorry {user_name}, we couldn't give you a 5-star experience. What can we do to make up for the {missing_stars} stars?"
+
+    # Reply to the review
+    reply_body = {
+        "comment": response
+    }
+    api_client.accounts().locations().reviews().reply(parent=f"accounts/{account_id}/locations/{location_id}/reviews/{review['reviewId']}", body=reply_body).execute()
+
