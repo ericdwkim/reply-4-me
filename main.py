e2a9bedcb75af88f256b0719cb3b640c6755380a
ericdwkim <ericdwkim.io@gmail.com>
2023-05-28 18:25:48 -0500

    API client initialization WIP

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ credentials = service_account.Credentials.from_service_account_file(
     scopes=['https://www.googleapis.com/auth/business.manage']
 )
 
-client = googleapiclient.discovery.build(serviceName='reply4me', version='v1', credentials=credentials)
-print(client)
+client = googleapiclient.discovery.build(serviceName='mybusiness', version='v4', credentials=credentials)
+# print(client)
+
+'''
+TODO:
+https://console.cloud.google.com/apis/api/mybusinessbusinessinformation.googleapis.com/metrics?project=fiery-caldron-388120&supportedpurview=project
+serviceName: mybusinessbusinessinformation.googleapis.com 
+version: v4
+'''
