8897d063072a3d96c687433397ca2891d8d7e170
ericdwkim <ericdwkim.io@gmail.com>
2023-05-29 15:14:39 -0500

    Waiting on increased quota limit approval. HttpError 429 for GET request due to quota limit of zero

--- main-jippity.py
+++ main-jippity.py
@@ -16,6 +16,7 @@ api_service = build("mybusinessbusinessinformation", "v1", credentials=creds, st
 
 # Fetch account_id, location_id from API
 
+
 accounts = api_service.accounts().list().execute()
 account_id = accounts['accounts'][0]['name']
 print(f'account id: {account_id}')
@@ -25,27 +26,27 @@ print(f'location id: {location_id}')
 
 
 # Fetch the reviews
-reviews = api_service.accounts().locations().reviews().list(parent=f"accounts/{account_id}/locations/{location_id}").execute()
-
-# Analyze sentiment and respond to reviews
-for review in reviews["reviews"]:
-    user_name = review["reviewer"]["displayName"]
-    num_of_stars_user_gave = review["starRating"]
-    review_text = review["comment"]
-
-    # Analyze sentiment using TextBlob
-    sentiment = TextBlob(review_text).sentiment.polarity
-    response = ""
-
-    if sentiment > 0 and num_of_stars_user_gave >= 4:
-        response = f"Thanks for the {num_of_stars_user_gave} stars, {user_name}! We hope you'll be back soon."
-    else:
-        missing_stars = 5 - num_of_stars_user_gave
-        response = f"Sorry {user_name}, we couldn't give you a 5-star experience. What can we do to make up for the {missing_stars} stars?"
-
-    # Reply to the review
-    reply_body = {
-        "comment": response
-    }
-    api_service.accounts().locations().reviews().reply(parent=f"accounts/{account_id}/locations/{location_id}/reviews/{review['reviewId']}", body=reply_body).execute()
-
+# reviews = api_service.accounts().locations().reviews().list(parent=f"accounts/{account_id}/locations/{location_id}").execute()
+#
+# # Analyze sentiment and respond to reviews
+# for review in reviews["reviews"]:
+#     user_name = review["reviewer"]["displayName"]
+#     num_of_stars_user_gave = review["starRating"]
+#     review_text = review["comment"]
+#
+#     # Analyze sentiment using TextBlob
+#     sentiment = TextBlob(review_text).sentiment.polarity
+#     response = ""
+#
+#     if sentiment > 0 and num_of_stars_user_gave >= 4:
+#         response = f"Thanks for the {num_of_stars_user_gave} stars, {user_name}! We hope you'll be back soon."
+#     else:
+#         missing_stars = 5 - num_of_stars_user_gave
+#         response = f"Sorry {user_name}, we couldn't give you a 5-star experience. What can we do to make up for the {missing_stars} stars?"
+#
+#     # Reply to the review
+#     reply_body = {
+#         "comment": response
+#     }
+#     api_service.accounts().locations().reviews().reply(parent=f"accounts/{account_id}/locations/{location_id}/reviews/{review['reviewId']}", body=reply_body).execute()
+#
